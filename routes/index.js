d30cee924e68c2daa926cb7730108156dee3df84
AMAL SHUKLA <shuklamal94@gmail.com>
2017-08-19 08:41:53 +0000

    Login and Signup Page UI Improved

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,6 @@ router.post("/register",function(req,res){
     var newUser = new User({username:req.body.username});
     User.register(newUser,req.body.password,function(err,user){
        if(err){
-           console.log(err);
            req.flash("error", err.message);
            return res.redirect('register');
        } 
